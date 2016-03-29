4ec3c21d9b43971147a9896f0e7d5fb3c7ba6db8
Brendan Kirchner <brendankirchner@me.com>
2016-03-29 15:29:05 -0400

    added example and framework

---
+++ ActivityView/ActivityView/ActivityView.h
@@ -0,0 +1,19 @@
+//
+//  ActivityView.h
+//  ActivityView
+//
+//  Created by Brendan Kirchner on 3/29/16.
+//  Copyright © 2016 Brendan Kirchner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ActivityView.
+FOUNDATION_EXPORT double ActivityViewVersionNumber;
+
+//! Project version string for ActivityView.
+FOUNDATION_EXPORT const unsigned char ActivityViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ActivityView/PublicHeader.h>
+
+
